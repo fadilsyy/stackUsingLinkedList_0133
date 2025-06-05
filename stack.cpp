4b13dcc7d2a8dc27ebdd93fe6a7a10fdc1793666
fadilsyy <fadilasytar@gmail.com>
2025-06-05 14:56:04 +0700

    ket. baru

--- stack.cpp
+++ stack.cpp
@@ -45,6 +45,8 @@ public:
         cout << "Popped value: " << top->data << endl;
         top = top->next; // update the top pointer to the next node
     }
+
+    // peak/top operation: retrieve the value of the topmost element without removing it
 };
 
 int main() {
