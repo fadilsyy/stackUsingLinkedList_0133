49de8a8576974114dab08232e11219fa238efd4d
fadilsyy <fadilasytar@gmail.com>
2025-06-05 14:36:43 +0700

    class baru

---
+++ stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Nod
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(){
+        next = NULL;
+    }
+};
+
+class Stack {
+    
+};
